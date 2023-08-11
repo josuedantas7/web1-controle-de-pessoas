d708f53d508e678ae2bf9e251c3e52f9487b5d01
Josué Dantas <ybadez@gmail.com>
2023-08-11 04:30:25 -0300

    update: comentando código

--- js/tela-aluno.js
+++ js/tela-aluno.js
@@ -1,42 +1,54 @@
+// Definição da chave utilizada para armazenar os dados dos alunos no localStorage
 const ALUNOS_KEY = "alunos";
 
-const alunosCount = document.querySelector("#alunos-count");
-const chartCanvas = document.querySelector("#chart");
+// Seleção de elementos do DOM
+const alunosCount = document.querySelector("#alunos-count"); // Elemento que exibe a contagem de alunos
+const chartCanvas = document.querySelector("#chart"); // Elemento do canvas para o gráfico
 
-let chart;
-let nextAlunoId = 1;
+let chart; // Variável para armazenar a instância do gráfico
 
+// Listener para mensagens recebidas via window.postMessage
 window.addEventListener("message", (event) => {
-  if (event.data && event.data.action === "alunoRemovido") {
-    const alunoIdRemovido = event.data.alunoId;
-    removeAlunoFromUI(alunoIdRemovido);
-    updateAlunosCount();
-    updateChart();
+  const alunoIdRemovido = event.data.alunoId; // Obtém o ID do aluno removido da mensagem
+  removeAlunoFromUI(alunoIdRemovido); // Chama a função para remover o aluno da interface
+  updateAlunosCount(); // Atualiza a contagem de alunos
+  updateChart(); // Atualiza o gráfico
   }
-});
+);
 
+// Função para remover um aluno da interface do usuário
 const removeAlunoFromUI = (alunoId) => {
-  const alunoElement = document.querySelector(`.aluno[data-id="${alunoId}"]`);
+  const alunoElement = document.querySelector(`.aluno[data-id="${alunoId}"]`); // Encontra o elemento do aluno pelo ID
   if (alunoElement) {
-    alunoElement.remove();
+    alunoElement.remove(); // Remove o elemento do aluno da interface
   }
 };
 
+// Função para atualizar a contagem de alunos exibida
 const updateAlunosCount = () => {
+  // Recupera os dados de alunos do localStorage ou cria um array vazio
   const totalAlunos = JSON.parse(localStorage.getItem(ALUNOS_KEY))?.length || 0;
-  alunosCount.textContent = totalAlunos;
+  alunosCount.textContent = totalAlunos; // Atualiza o elemento da contagem
 };
 
+// Função para atualizar o gráfico de grupos musculares
 const updateChart = () => {
+  // Recupera os dados de alunos do localStorage ou cria um array vazio se não houver dados
   const alunos = JSON.parse(localStorage.getItem(ALUNOS_KEY)) || [];
+
+  // Objeto para armazenar a contagem de cada grupo muscular
   const musculosCount = {};
 
+  // Loop pelos dados de cada aluno
   for (let i = 0; i < alunos.length; i++) {
     const alunoData = alunos[i];
     const membros = alunoData.membros;
 
+    // Loop pelos grupos musculares de cada aluno
     for (let j = 0; j < membros.length; j++) {
       const membro = membros[j];
+
+      // Atualiza a contagem no objeto musculosCount
       if (musculosCount[membro]) {
         musculosCount[membro]++;
       } else {
@@ -45,23 +57,30 @@ const updateChart = () => {
     }
   }
 
+  // Extrai os nomes dos grupos musculares
   const musculos = Object.keys(musculosCount);
+
+  // Cria um array com as contagens dos grupos musculares
   const countData = musculos.map((membro) => musculosCount[membro]);
 
+  // Verifica se já existe uma instância do gráfico
   if (chart) {
-    chart.data.labels = musculos;
-    chart.data.datasets[0].data = countData;
-    chart.update();
+    // Se existir, atualiza os dados do gráfico
+    chart.data.labels = musculos; // Atualiza os rótulos do gráfico
+    chart.data.datasets[0].data = countData; // Atualiza os dados dos datasets do gráfico
+    chart.update(); // Atualiza o gráfico
   } else {
-    const ctx = chartCanvas.getContext("2d");
+    // Caso contrário, cria uma nova instância do gráfico
+    const ctx = chartCanvas.getContext("2d"); // Obtém o contexto de renderização
     chart = new Chart(ctx, {
-      type: "pie",
+      type: "pie", // Define o tipo de gráfico como "pie" (pizza)
       data: {
-        labels: musculos,
+        labels: musculos, // Define os rótulos do gráfico
         datasets: [
           {
-            data: countData,
+            data: countData, // Define os dados dos datasets do gráfico
             backgroundColor: [
+              // Define as cores de fundo para cada fatia do gráfico
               "#FF6384",
               "#36A2EB",
               "#FFCE56",
@@ -74,16 +93,14 @@ const updateChart = () => {
         ],
       },
       options: {
-        responsive: true,
-        maintainAspectRatio: false,
+        responsive: true, // Permite que o gráfico seja responsivo
+        maintainAspectRatio: false, // Permite que o tamanho do gráfico seja ajustado
       },
     });
   }
 };
 
-updateAlunosCount();
-updateChart();
-
+// Listener para o evento de mudança de storage (localStorage)
 window.addEventListener("storage", () => {
   updateAlunosCount();
   updateChart();
--- js/tela-atendente.js
+++ js/tela-atendente.js
@@ -1,32 +1,38 @@
+// Definição da chave utilizada para armazenar os dados dos alunos no localStorage
 const ALUNOS_KEY = "alunos";
 
-const alunoForm = document.querySelector("#formulario");
-const alunoInput = document.querySelector("#formulario-submit-input");
-const checkboxes = document.querySelectorAll('input[name="membro"]');
-const limparAlunosButton = document.querySelector("#limpar-alunos");
+// Seleção de elementos do DOM
+const alunoForm = document.querySelector("#formulario"); // Formulário de adição de aluno
+const alunoInput = document.querySelector("#formulario-submit-input"); // Campo de entrada do nome do aluno
+const checkboxes = document.querySelectorAll('input[name="membro"]'); // Checkboxes dos grupos musculares
+const limparAlunosButton = document.querySelector("#limpar-alunos"); // Botão de limpar todos os alunos
 
+// Variável para controlar o próximo ID de aluno
 let nextAlunoId = 1;
 
+// Função para salvar informações de um aluno
 const saveAluno = (text, membros) => {
+  // Criação de objeto com informações do aluno
   const alunoData = {
-    id: nextAlunoId,
-    nome: text,
-    membros: membros,
+    id: nextAlunoId, // Atribuição do próximo ID disponível
+    nome: text, // Nome do aluno
+    membros: membros, // Grupos musculares selecionados
   };
 
-  nextAlunoId++; // Incremento do ID
+  nextAlunoId++; // Incremento do próximo ID disponível
 
   if (membros.length === 0) {
     alert("Selecione pelo menos um grupo muscular");
     return;
   }
 
+  // Recuperação dos dados de alunos do localStorage ou criação de um array vazio
   let alunos = JSON.parse(localStorage.getItem(ALUNOS_KEY)) || [];
   alunos.push(alunoData);
   localStorage.setItem(ALUNOS_KEY, JSON.stringify(alunos));
 
+  // Criação de elementos DOM para exibir informações do aluno
   const alunoList = document.querySelector("#aluno-list");
-
   const aluno = document.createElement("div");
   aluno.classList.add("aluno");
   aluno.setAttribute("data-id", alunoData.id);
@@ -42,23 +48,26 @@ const saveAluno = (text, membros) => {
   const deleteBtn = document.createElement("button");
   deleteBtn.classList.add("remove-aluno");
   deleteBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+
+  // Listener para remover aluno e atualizar contagens após clique no botão de remoção
   deleteBtn.addEventListener("click", () => {
     aluno.remove();
     removeAluno(alunoData.id);
     updateAlunosCount();
     window.parent.postMessage({ action: "alunoRemovido" }, "*");
   });
-  aluno.appendChild(deleteBtn);
 
+  aluno.appendChild(deleteBtn);
   alunoList.appendChild(aluno);
 
+  // Limpar campo de entrada e desmarcar checkboxes
   alunoInput.value = "Aluno ";
   alunoInput.focus();
 
   updateAlunosCount();
-  desmarcarCheckbox();
 };
 
+// Função para atualizar a contagem de alunos exibida
 const updateAlunosCount = () => {
   const totalAlunos = document.querySelector("#aluno-list").children.length;
   document.querySelectorAll("#alunos-count").forEach((element) => {
@@ -66,12 +75,14 @@ const updateAlunosCount = () => {
   });
 };
 
+// Função para desmarcar todas as checkboxes dos grupos musculares
 const desmarcarCheckbox = () => {
   checkboxes.forEach((checkbox) => {
     checkbox.checked = false;
   });
 };
 
+// Listener para o botão de limpar todos os alunos
 limparAlunosButton.addEventListener("click", function () {
   if (confirm("Tem certeza que deseja remover todos os alunos?")) {
     removerTodosAlunos();
@@ -82,34 +93,39 @@ limparAlunosButton.addEventListener("click", function () {
   }
 });
 
+// Função para remover todos os alunos do localStorage
 function removerTodosAlunos() {
   localStorage.removeItem(ALUNOS_KEY);
 }
 
+// Função para remover um aluno pelo ID
 const removeAluno = (id) => {
-  let alunos = JSON.parse(localStorage.getItem(ALUNOS_KEY)) || [];
+  let alunos = JSON.parse(localStorage.getItem(ALUNOS_KEY));
   const alunoIndex = alunos.findIndex((aluno) => aluno.id === id);
-  if (alunoIndex !== -1) {
-    alunos.splice(alunoIndex, 1);
-    localStorage.setItem(ALUNOS_KEY, JSON.stringify(alunos));
-    window.parent.postMessage({ action: "alunoRemovido", alunoId: id }, "*");
-  }
+  alunos.splice(alunoIndex, 1);
+  localStorage.setItem(ALUNOS_KEY, JSON.stringify(alunos));
+  window.parent.postMessage({ action: "alunoRemovido", alunoId: id }, "*");
 };
 
+// Listener para o formulário de adição de aluno
 alunoForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const valorInput = alunoInput.value;
 
   if (valorInput) {
+    // Seleciona checkboxes marcadas e obtém seus valores
     const membrosSelecionados = Array.from(
       document.querySelectorAll('input[name="membro"]:checked')
     ).map((checkbox) => checkbox.value);
+
+    // Chama a função para salvar o aluno com as informações fornecidas
     saveAluno(valorInput, membrosSelecionados);
     desmarcarCheckbox();
   }
 });
 
+// Atualização da contagem de alunos ao carregar a página de visualização dos alunos
 if (window.location.pathname.includes("tela-aluno.html")) {
   updateAlunosCount();
 }
